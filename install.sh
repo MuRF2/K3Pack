fa1f9a18341c538eab1e37c38358a107622c37fd
MuRF2 <michi.lueders@t-online.de>
2022-07-24 19:39:57 +0200

    requirements

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git clone https://github.com/MuRF2/K3Pack
 cd ./K3Pack
 python3 -m venv K3Pack-env
 source ./K3Pack-env/bin/activate
-pip install pyinstaller
+pip install -r requirements.txt
 pyinstaller --onefile main.py
 mv ./dist/main ./K3Pack
 deactivate
