78fdb89619fa998d953a1aeb5957984b9e002eb9
MuRF2 <michi.lueders@t-online.de>
2022-07-24 19:37:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# K3Pack
+
+## install
+
+```
+curl https://raw.githubusercontent.com/MuRF2/K3Pack/master/install.sh | bash
+```
+
---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/MuRF2/K3Pack
+cd ./K3Pack
+python3 -m venv K3Pack-env
+source ./K3Pack-env/bin/activate
+pip install pyinstaller
+pyinstaller --onefile main.py
+mv ./dist/main ./K3Pack
+deactivate
+
