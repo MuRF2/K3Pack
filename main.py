fb624ea20dec600aaf20d6accf1bfffcb42a7700
MuRF2 <michi.lueders@t-online.de>
2022-07-25 02:27:02 +0200

    ui

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import getpass
 import argparse
-import requests
-
+import filecmp
+from requests import get
 
 version_number = '0.1'
-url=""
+g_url = "https://raw.githubusercontent.com/MuRF2/K3Pack/master/packages_list"
+g_package_list_file_path = '/home/' + getpass.getuser() + '/.k3pack/package_list_file'
+
+
+def modify_package_list_file(file_path):
+    lines = ['Readme', 'How to write text files in Python']
+    with open(file_path, 'a') as f:
+        for line in lines:
+            f.write(line)
+            f.write('\n')
+
+
+def compare(f1, f2):
+    return filecmp.cmp(f1, f2, shallow=False)
 
 
 def arguments():
@@ -17,18 +31,43 @@ def arguments():
                                                             "list-available", "update"],
                         help="main operator to be selected")
     args = parser.parse_args()
-    print(args.main_operator)
-
-    return parser.parse_args()
+    return args
 
 
 class PackageList:
+
+    @staticmethod
+    def download(file_path, url):
+        with open(file_path, "wb") as file:
+            response = get(url)
+            file.write(response.content)
+
+    @staticmethod
+    def load(file_path):
+        return
+
     def __init__(self):
-        return
+        self.__c_url = None
+        self.__c_package_list_file_path = None
+        self.__c_package_list_file = None
 
-    def download(self, url, filename):
-        return
+    def set_file_path(self, path):
+        self.__c_package_list_file_path = path
 
+    def get_file_path(self):
+        return self.__c_package_list_file_path
+
+    def set_url(self, url):
+        self.__c_url = url
+
+    def get_url(self):
+        return self.__c_url
+
+    def set_file(self, file):
+        self.__c_package_list_file = file
+
+    def get_file(self):
+        return self.__c_package_list_file
 
 
 def install(operator):
@@ -36,7 +75,24 @@ def install(operator):
 
 
 if __name__ == '__main__':
-    print(arguments())
+    parsed_argument = arguments()
+
+    ThePackageList = PackageList()
+    ThePackageList.set_file_path(g_package_list_file_path)
+    ThePackageList.set_url(g_url)
+
+    if parsed_argument.main_operator == 'update':
+        ThePackageList.download(ThePackageList.get_file_path(), ThePackageList.get_url())
+    elif parsed_argument.main_operator == 'install':
+        print('install')
+    elif parsed_argument.main_operator == 'uninstall':
+        print('uninstall')
+    elif parsed_argument.main_operator == 'list-installed':
+        print('list-installed')
+    elif parsed_argument.main_operator == 'list-available':
+        print('list-available')
+
+
 
 
 
