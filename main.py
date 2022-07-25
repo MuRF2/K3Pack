c794a4f532abfc121e0b63311362a04cf2f15285
MuRF2 <michi.lueders@t-online.de>
2022-07-25 14:40:16 +0200

    install argpars

--- main.py
+++ main.py
@@ -21,13 +21,22 @@ def create_folder(path):
 
 def arguments():
     parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
-                        help='K3Pack is an easy to use packet manager written in Python. '
-                             'Actually just a fun project.')
-    parser.add_argument('-v', '--version', action='version', default=argparse.SUPPRESS,
-                        version="Version {} is installed.".format(version_number), help='show version number')
-    parser.add_argument("main_operator", type=str, choices=["install", "uninstall", "list-installed",
-                                                            "list-available", "update"],
+    parser.add_argument('-h', '--help',
+                        action='help',
+                        default=argparse.SUPPRESS,
+                        help='K3Pack is an easy to use packet manager written in Python. Actually just a fun project.')
+    parser.add_argument('-v', '--version',
+                        action='version',
+                        default=argparse.SUPPRESS,
+                        version="Version {} is installed.".format(version_number),
+                        help='show version number')
+    parser.add_argument('-p', '--package',
+                        type=str,
+                        nargs=1,
+                        help="package name for install and uninstall argument")
+    parser.add_argument("main_operator",
+                        type=str,
+                        choices=["install", "uninstall", "list-installed", "list-available", "update"],
                         help="main operator to be selected")
     return parser.parse_args()
 
@@ -51,7 +60,7 @@ def download(file_path, url):
             response = get(url)
             file.write(response.content)
         except requests.exceptions.ConnectionError as e:
-            print('An error occurred during packet list download.')
+            print('An error occurred during download.')
             raise SystemExit(e)
 
 
@@ -65,16 +74,21 @@ def parse_json_file(path):
         print('File not found, maybe you need to update the package list first?')
 
 
-def load_dictionary_content(dictionary, part):
-    return
-
-
 def install(package_name):
-    return "test"
+    packages = parse_json_file(g_package_list_file_path)
+    available_flag = False
+    size = len(packages.items())
+    for x in range(0, size):
+        value = list(packages.items())[x][1]
+        for y in value:
+            if y['name'] == package_name:
+                available_flag = True
+    if not available_flag:
+        print("Package could not be found. List all available packages with ./k3pack list-available")
 
 
 def list_available():
-    return
+    print_package_names(parse_json_file(g_package_list_file_path))
 
 
 def print_package_names(dictionary):
@@ -85,38 +99,30 @@ def print_package_names(dictionary):
             print(y['name'] + " version: " + str(y['version']))
 
 
+def print_my_dictionary_value(dictionary, part):
+    size = len(dictionary.items())
+    for x in range(0, size):
+        value = list(dictionary.items())[x][1]
+        for y in value:
+            print(y[part])
+
 
 if __name__ == '__main__':
 
     if arguments().main_operator == 'update':
         refresh_package_list(g_package_list_file_path, g_package_list_file_url)
     elif arguments().main_operator == 'install':
-        print('install')
+        create_folder(g_install_folder_path)
+        install(arguments().package.pop())
     elif arguments().main_operator == 'uninstall':
         print('uninstall')
     elif arguments().main_operator == 'list-installed':
         print('list-installed')
     elif arguments().main_operator == 'list-available':
-        print_package_names(parse_json_file(g_package_list_file_path))
+        list_available()
 
     # create folder for installations
     # create_folder(g_install_folder_path)
-
-    #print(data["package1"])
-    #print(data["package2"])
-
-
-
-   # print_package_name(data["package1"])
-   # print_package_name(data["package2"])
-   # print_package_name(data["package3"])
-
-   # for i in data['package1']:
-    #    print("name:", i['name'])
-    #    print("url:", i['url'])
-     #   print("version:", i['version'])
-    #    print("various", i['various'])
-     #   print()
 
 
 
