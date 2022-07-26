0e99d750ba9d05e6a91183e9608c1432278a220c
MuRF2 <michi.lueders@t-online.de>
2022-07-26 02:45:08 +0200

    giga chad changes

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 git clone https://github.com/MuRF2/K3Pack
-cd ./K3Pack
+cd ./K3Pack || exit
 python3 -m venv K3Pack-env
 source ./K3Pack-env/bin/activate
 pip install -r requirements.txt
 pyinstaller --onefile main.py
-mkdir -p /home/$USER/.k3pack
+mkdir -p /home/"$USER"/.k3pack
 mv ./dist/main ./k3pack
-mv ./k3pack /home/$USER/.k3pack
+mv ./k3pack /home/"$USER"/.k3pack
 deactivate
 rm -r ../K3Pack
 
--- main.py
+++ main.py
@@ -4,19 +4,13 @@ import argparse
 import requests
 from requests import get
 import json
+import re
 
 version_number = '0.1'
 g_package_list_file_url = "https://raw.githubusercontent.com/MuRF2/K3Pack/master/packages_list.json"
 g_package_list_file_path = '/home/' + getpass.getuser() + '/.k3pack/package_list_file.json'
-g_installed_list_file_path = '/home/' + getpass.getuser() + '/.k3pack/installed_list_file'
-g_install_folder_path = '/home/' + getpass.getuser() + '/.k3pack/installed'
-
-
-def create_folder(path):
-    try:
-        os.mkdir(path)
-    except OSError as error:
-        print(error)
+g_installed_list_file_path = '/home/' + getpass.getuser() + '/.k3pack/installed/installed_list_file'
+g_install_folder_path = '/home/' + getpass.getuser() + '/.k3pack/installed/'
 
 
 def arguments():
@@ -41,12 +35,6 @@ def arguments():
     return parser.parse_args()
 
 
-def refresh_package_list(file_path: str, url: str):
-    delete_file(file_path)
-    download(file_path, url)
-    print('Package list updated.')
-
-
 def delete_file(path):
     try:
         os.remove(path)
@@ -54,11 +42,19 @@ def delete_file(path):
         pass
 
 
+def create_folder(path):
+    try:
+        os.mkdir(path)
+    except OSError as error:
+        print(error)
+
+
 def download(file_path: str, url: str):
     """
-
-    :param file_path:
-    :param url:
+    This function downloads and creates a file. The file location and download url is passed to the function.
+    The filename is part of the path.
+    :param file_path: storage location of new created file - type: str
+    :param url: for get request - type:str
     """
     with open(file_path, "wb") as file:
         try:
@@ -85,7 +81,7 @@ def parse_json_file(path: str) -> dict:
     }
     The output dictionary would be:
     {'package1': [{'name': 'HelloWorld', 'url': 'https://yoururl.com', 'version': 1, 'various': '-'}]}
-    :param path:
+    :param path: storage location of json file
     :return: for k3pack parsed dictionary
     """
     try:
@@ -110,23 +106,44 @@ def get_package_url(dictionary: dict, package_name: str) -> str:
                 return y['url']
 
 
-def install(package_name):
-    d = parse_json_file(g_package_list_file_path)
-    url = get_package_url(d, package_name)
-    if url is None:
-        print('ERROR: Package could not be found. List all available packages with ./k3pack list-available')
-    if url is not None:
-        print(url)
+def get_package_name(dictionary):
+    for x in range(0, len(dictionary.items())):
+        for y in list(dictionary.items())[x][1]:
+            return y['name']
 
 
-def list_available():
-    print_package_names_and_version(parse_json_file(g_package_list_file_path))
+def get_package_name_from_url(url):
+    return re.findall('([^\/]+)\/?$', url).pop()
 
 
 def print_package_names_and_version(dictionary):
     for x in range(0, len(dictionary.items())):
         for y in list(dictionary.items())[x][1]:
             print(y['name'] + " version: " + str(y['version']))
+
+
+def refresh_package_list(file_path: str, url: str):
+    delete_file(file_path)
+    download(file_path, url)
+    print('Package list updated.')
+
+
+def install(package_name):
+    d = parse_json_file(g_package_list_file_path)
+    url = get_package_url(d, package_name)
+    if url is None:
+        print('ERROR: Package could not be found. List all available packages with ./k3pack list-available')
+    if url is not None:
+        if os.path.exists(g_install_folder_path + get_package_name_from_url(url)) is True:
+            # check version in installed_package_file
+            print("File / program already installed.")
+        else:
+            download(g_install_folder_path + get_package_name_from_url(url), url)
+            print("downloaded...")
+
+
+def list_available():
+    print_package_names_and_version(parse_json_file(g_package_list_file_path))
 
 
 if __name__ == '__main__':
@@ -138,7 +155,6 @@ if __name__ == '__main__':
             install(arguments().package.pop())
         except AttributeError as error:
             print('ERROR: parameter install needs another attribute')
-
     elif arguments().main_operator == 'uninstall':
         print('uninstall')
     elif arguments().main_operator == 'list-installed':
